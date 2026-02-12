8ffdbfef87483dda9411f546ad7f618167ff27e1
Shulamit Petranker <shulamitp@altshare.com>
2026-02-12 12:01:12 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+// Hello endpoint
+app.MapGet("/hello", () => "Hello from .NET API!")
+   .WithName("GetHello");
+
+// Root endpoint
+app.MapGet("/", () => "Welcome to Hello API - try /hello endpoint")
+   .WithName("Root");
+
+app.Run();
